8f975212b274751340e5e44338d8a8847b0e1118
Zixuan Feng <dl349799020@outlook.com>
2020-12-30 14:53:31 +0800

    add example answer

--- q5.cpp
+++ q5.cpp
@@ -42,3 +42,37 @@ string computeLongestStr(string s, int back, int front){
 
     return s.substr(back, front-back+1);
 }
+
+// example answer
+// class Solution {
+// public:
+//     pair<int, int> expandAroundCenter(const string& s, int left, int right) {
+//         while (left >= 0 && right < s.size() && s[left] == s[right]) {
+//             --left;
+//             ++right;
+//         }
+//         return {left + 1, right - 1};
+//     }
+
+//     string longestPalindrome(string s) {
+//         int start = 0, end = 0;
+//         for (int i = 0; i < s.size(); ++i) {
+//             auto [left1, right1] = expandAroundCenter(s, i, i);
+//             auto [left2, right2] = expandAroundCenter(s, i, i + 1);
+//             if (right1 - left1 > end - start) {
+//                 start = left1;
+//                 end = right1;
+//             }
+//             if (right2 - left2 > end - start) {
+//                 start = left2;
+//                 end = right2;
+//             }
+//         }
+//         return s.substr(start, end - start + 1);
+//     }
+// };
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
